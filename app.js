3a860159404bff2e0c061ad994a37186c59d087a
carson ross <carson.yaumy@gmail.com>
2017-07-15 22:20:14 +0800

    minimalist

--- app.js
+++ app.js
@@ -348,25 +348,25 @@ function callback_comp_name(compname) {
 //Stock quote
 function stockcard(session,name,pic,ticker,exc) {
     return [ 
-        new builder.HeroCard(session)
-        .images([
-            builder.CardImage.create(session, pic)
-        ])
+//         new builder.HeroCard(session)
+//         .images([
+//             builder.CardImage.create(session, pic)
+//         ])
 
-,
+// ,
 
         new builder.HeroCard(session)
         .title( name  + " (" + ticker +")")
         .subtitle("Exchange: " + exc)
 
-        .text("Please choose from the following options for more detailed analysis")
+        .text("Please choose from the following options for the sentiment analysis")
         .buttons([
+            builder.CardAction.postBack(session, '4', 'Show Market Sentiment statistics')
             builder.CardAction.openUrl(session, 'http://www.cnbc.com/quotes/?symbol=' + ticker, 'Stock Performance'),
             builder.CardAction.postBack(session, '1', 'See supporting news articles'),
             builder.CardAction.postBack(session, '2', 'See trending most mentioned topics'),
             builder.CardAction.postBack(session, '3', 'See co-mentioned companies'),
-            builder.CardAction.postBack(session, '4', 'Show Market Sentiment statistics')
-    
+            
         ])
         
     ]
